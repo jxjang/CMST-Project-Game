390ef045d9eefc3084cd791278771483dd0ec279
jxjang <121998013+jxjang@users.noreply.github.com>
2023-02-22 13:36:04 +0900

    night

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,44 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private List<EnemyBasic> enemyTypes= new List<EnemyBasic>();
+    private int TimeScore;
+    private List<Vector3> initlocs= new List<Vector3>();
+
+    private int ybound = 20;
+    private int xbound = 15;
+    private int ylow = 6;
     void Start()
     {
-        
+        enemyTypes.Add(new SuicideEnemy());
+        enemyTypes.Add(new LingeringEnemy());
+        enemyTypes.Add(new TransitoryEnemy());
+
+        TimeScore = 0;
+
+        for (int i = -xbound; i <= xbound; i++)
+        {
+            initlocs.Add(new Vector3(i, ybound));
+        }
+
+        for (int j = ylow; j <= ybound; j++)
+        {
+            initlocs.Add(new Vector3(-xbound, j));
+            initlocs.Add(new Vector3(xbound, j));
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+
+    }
+
+    int SpawnGen()
+    {
+        return Random.Range(0, 2);
     }
 }
